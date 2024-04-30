759046b19bdb7878524033a81b04a519f62c37e9
Faiz Muhammad Shah <tofeequeshah456@gmail.com>
2024-05-01 00:29:03 +0500

    Add marksheet grading system using TypeScript, Node.js, and Inquirer
    Implemented a marksheet grading system using TypeScript and Inquirer library to prompt users for their obtained marks and calculate their grades based on predefined criteria. Added functionality to handle various grade ranges and provide appropriate feedback to the user.

---
+++ main.ts
@@ -0,0 +1,44 @@
+import inquirer from "inquirer"
+
+const answers = await inquirer.prompt([
+    {
+        name: "stdName",
+        type: "string",
+        message: "Please enter your name:"
+    },
+    {
+        name: "obtMarks",
+        type: "number",
+        message: "Please enter your obtained marks:"
+    },
+    {
+        name: "maxMarks",
+        type: "number",
+        message: "Please enter maximum marks:"
+    }
+]);
+
+let percentage = (answers.obtMarks/answers.maxMarks)*100;
+
+if (percentage >= 80 && percentage <100) {
+    console.log(`Outstanding! ${answers.stdName}, Your percentage is ${percentage}, and your grade is A1.`)
+}
+else if(percentage>=70 && percentage<80) {
+    console.log(`Excellent! ${answers.stdName}, Your percentage is ${percentage}, and your grade is A.`)
+}
+else if(percentage>=60 && percentage<70) {
+    console.log(`Very Good! ${answers.stdName}, Your percentage is ${percentage}, and your grade is AB.`)
+}
+else if(percentage>=50 && percentage<60) {
+    console.log(`Congratulations! ${answers.stdName}, Your percentage is ${percentage}, and your grade is B.`)
+
+}
+else if(percentage>=40 && percentage<50) {
+    console.log(`Dear! ${answers.stdName}, Your percentage is ${percentage}, and your grade is C.`)
+}
+else if(percentage>=33 && percentage<40) {
+    console.log(`Dear! ${answers.stdName}, Your percentage is ${percentage}, and your grade is D.`)
+}
+else {
+    console.log(`Sorry! Your percentage is ${percentage}, and you are fail. `)
+}
